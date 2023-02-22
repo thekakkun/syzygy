920a7ffbc23a683bf035c1e2bf572b34a42e4da3
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-02-22 13:59:16 -0500

    Initialize rust wasm library

---
+++ libs/sygyzy_gcm/src/lib.rs
@@ -0,0 +1,26 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, world!");
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
